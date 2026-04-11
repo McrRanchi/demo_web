baa776d0981ad31c8a10944d231508d604418348
McrRanchi <raj98355_kumar@rediff.com>
2026-04-11 12:19:08 +0530

    Enhance web page with sidebar and loading spinner
    Added sidebar navigation and loading spinner to the web page.

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import time
+#st.markdown("""<style>*{background-color:white;}</style>""",unsafe_allow_html=True)
+with st.spinner("Loading...."):
+    time.sleep(5)
+
 st.title("My First web page on GitHub!!!")
+    
 st.balloons()
+with st.sidebar:
+    st.success("Home")
+    st.success("About Us")
+    st.success("Account")
+    st.success("Login")
+        
+    with st.spinner("Loading ....."):
+        time.sleep(5)
+    st.success("Done")
+
